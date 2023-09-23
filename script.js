aa06dfc14a3f2a702e428d00c2f4c91fdfd3a29c
Ada Ma <mayanran194@gmail.com>
2023-09-23 14:51:44 -0500

    frontend

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const controller = document.querySelector('input[type=range]');
 const radialProgress = document.querySelector('.RadialProgress');
+const progressValue = document.querySelector('.progress-value');
 
 const setProgress = (progress) => {
     const value = `${progress}%`;
-    radialProgress.style.setProperty('--progress', value)
-    radialProgress.innerHTML = value
+    let angleDegree = value + "deg";
+    progressValue.textContent = `${progress}%`
+    radialProgress.style.background = `conic-gradient(#7d2ae8 ${progress*3.6}deg, #ededed 0deg) `
+    //radialProgress.innerHTML = value
     radialProgress.setAttribute('aria-valuenow', value)
 }
 
