28a0c4c8e59c7cf3ab0bcd6de09550b34a2187bd
王淏天 <bruce@wanghaotiandeMacBook-Pro.local>
2023-09-23 20:12:57 -0500

    new pages

---
+++ frontend/Physical.js
@@ -0,0 +1,39 @@
+const controller = document.querySelector('input[type=range]');
+const radialProgress = document.querySelector('.PhysicalProgress');
+const progressValue = document.querySelector('.progress-value');
+
+var value = 0;
+const setProgress = (progress) => {
+    value = `${progress}%`;
+    let angleDegree = value + "deg";
+    progressValue.textContent = `${progress}%`
+    radialProgress.style.background = `conic-gradient(#7d2ae8 ${progress*3.6}deg, #ededed 0deg) `
+    //radialProgress.innerHTML = value
+    radialProgress.setAttribute('aria-valuenow', value)
+}
+
+setProgress(controller.value)
+controller.oninput = () => {
+    setProgress(controller.value)
+}
+
+document.getElementById('savePhysicalBtn').addEventListener('click', function () {
+    console.log("sent request!");
+    console.log(value);
+    let intValue = parseInt(value.slice(0, -1));
+    console.log(intValue);
+    fetch('/user/savePhysicalData', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: intValue
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+        .then(data => console.log(data))
+        .catch(error => console.log('Fetch error: ' + error.message));
+});
